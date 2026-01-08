f247ae498a2d5501bdeeb8194aad5944af10563a
Notheisz Mátyás <nodomatyi@gmail.com>
2026-01-08 13:10:49 +0100

    update register/login page to match existing code

--- src/routes/demo/lucia/login/+page.server.ts
+++ src/routes/demo/lucia/login/+page.server.ts
@@ -1,5 +1,4 @@
 import { hash, verify } from '@node-rs/argon2';
-import { encodeBase32LowerCase } from '@oslojs/encoding';
 import { fail, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import * as auth from '$lib/server/auth';
@@ -20,79 +19,98 @@ export const actions: Actions = {
 		const username = formData.get('username');
 		const password = formData.get('password');
 
-		if (!validateUsername(username)) {
-			return fail(400, { message: 'Invalid username (min 3, max 31 characters, alphanumeric only)' });
+		let isEmailLogin = false;
+
+		// verify wether username is an actual username or email
+		if (validateEmail(username)) {
+			isEmailLogin = true;
+		} else if (!validateUsername(username)) {
+			return fail(400, { message: 'Invalid username' });
+		} else {
+			isEmailLogin = false;
 		}
+
 		if (!validatePassword(password)) {
 			return fail(400, { message: 'Invalid password (min 6, max 255 characters)' });
 		}
 
 		const results = await db
 			.select()
-			.from(table.user)
-			.where(eq(table.user.username, username));
+			.from(table.User)
+			.where(isEmailLogin ? eq(table.User.email, username) : eq(table.User.username, username));
 
 		const existingUser = results.at(0);
 		if (!existingUser) {
 			return fail(400, { message: 'Incorrect username or password' });
 		}
 
-		const validPassword = await verify(existingUser.passwordHash, password, {
+		const validPassword = await verify(existingUser.passhash, password, {
 			memoryCost: 19456,
 			timeCost: 2,
 			outputLen: 32,
-			parallelism: 1,
+			parallelism: 1
 		});
 		if (!validPassword) {
 			return fail(400, { message: 'Incorrect username or password' });
 		}
 
-		const sessionToken = auth.generateSessionToken();
-		const session = await auth.createSession(sessionToken, existingUser.id);
-		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+		const userAgent = event.request.headers.get('user-agent') ?? 'unknown';
+
+		// todo: might needa error proof this later
+		const session = await auth.createSession(existingUser.id, userAgent);
+
+		// @ts-expect-error createSession might return null but physically cant since it returns an insert
+		auth.setSessionTokenCookie(event, session.id, session.eat);
 
 		return redirect(302, '/demo/lucia');
 	},
 	register: async (event) => {
 		const formData = await event.request.formData();
 		const username = formData.get('username');
+		const email = formData.get('email');
 		const password = formData.get('password');
 
 		if (!validateUsername(username)) {
 			return fail(400, { message: 'Invalid username' });
 		}
+		if (!validateEmail(email)) {
+			return fail(400, { message: 'Invalid email' });
+		}
 		if (!validatePassword(password)) {
 			return fail(400, { message: 'Invalid password' });
 		}
 
-		const userId = generateUserId();
 		const passwordHash = await hash(password, {
 			// recommended minimum parameters
 			memoryCost: 19456,
 			timeCost: 2,
 			outputLen: 32,
-			parallelism: 1,
+			parallelism: 1
 		});
 
 		try {
-			await db.insert(table.user).values({ id: userId, username, passwordHash });
+			const result = await db
+				.insert(table.User)
+				.values({ username, passhash: passwordHash, email, role: 'User' })
+				.returning({ id: table.User.id });
 
-			const sessionToken = auth.generateSessionToken();
-			const session = await auth.createSession(sessionToken, userId);
-			auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+			const userId = result[0]?.id;
+			if (!userId) {
+				return fail(500, { message: 'Failed to create user' });
+			}
+
+			const userAgent = event.request.headers.get('user-agent') ?? 'unknown';
+
+			const session = await auth.createSession(userId, userAgent);
+
+			// @ts-expect-error createSession might return null but physically cant since it returns an insert
+			auth.setSessionTokenCookie(event, session.id, session.eat);
 		} catch {
 			return fail(500, { message: 'An error has occurred' });
 		}
 		return redirect(302, '/demo/lucia');
-	},
+	}
 };
-
-function generateUserId() {
-	// ID with 120 bits of entropy, or about the same as UUID v4.
-	const bytes = crypto.getRandomValues(new Uint8Array(15));
-	const id = encodeBase32LowerCase(bytes);
-	return id;
-}
 
 function validateUsername(username: unknown): username is string {
 	return (
@@ -104,9 +122,10 @@ function validateUsername(username: unknown): username is string {
 }
 
 function validatePassword(password: unknown): password is string {
-	return (
-		typeof password === 'string' &&
-		password.length >= 6 &&
-		password.length <= 255
-	);
+	return typeof password === 'string' && password.length >= 6 && password.length <= 255;
+}
+
+function validateEmail(email: unknown): email is string {
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return typeof email === 'string' && regex.test(email);
 }
