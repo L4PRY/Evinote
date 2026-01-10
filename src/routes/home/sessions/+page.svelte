268b2429b71448878bb55d6447ab9cf55c08916b
Notheisz Mátyás <nodomatyi@gmail.com>
2026-01-10 09:30:38 +0100

    add rudimentary sessions view

--- src/routes/home/sessions/+page.svelte
+++ src/routes/home/sessions/+page.svelte
@@ -1,1 +1,20 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { PageServerData } from './$types';
+
+	let { data }: { data: PageServerData } = $props();
+</script>
+
 <h1>hi</h1>
+<div>here are ur sessions</div>
+{#each data.sessions as session (session.id)}
+	<div>
+		<p>Session ID: {session.id}</p>
+		<p>Created At: {new Date(session.iat).toLocaleString()}</p>
+		<p>User Agent: {session.description}</p>
+		<form method="post" action="?/invalidate" use:enhance>
+			<input type="hidden" name="session_id" value={session.id} />
+			<button>Sign out of this session</button>
+		</form>
+	</div>
+{/each}
