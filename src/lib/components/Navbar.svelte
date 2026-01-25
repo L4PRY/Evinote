1a049d0ea2a35bceffb0015b8e2dac7937aa8934
Patrik <patrikdravucz@gmail.com>
2026-01-25 20:22:36 +0100

    begin css & nav

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<nav>
+    <div>
+        
+    </div>
+</nav>
+
+<style>
+    div {
+        height: 60px;
+        background-color: var(--placeholder-bg-color);
+        width: 100%;
+    }
+</style>
