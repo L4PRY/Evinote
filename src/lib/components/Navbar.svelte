7310064dfd6a0a9abfd6a0e56ab8afd54f2883f4
Patrik <patrikdravucz@gmail.com>
2026-01-26 00:21:24 +0100

    insane navbar progess

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,14 +3,52 @@
 
 <nav>
     <div>
-        
+        <button>Explore</button>
+        <button>Dashboard</button>
+        <button>Settings</button>
     </div>
+	
 </nav>
 
 <style>
+	nav {
+		display: flex;
+        box-sizing: border-box;
+		align-items: center;
+		justify-content: center;
+		height: 50px;
+		background-color: transparent;
+		width: 100%;
+		position: absolute;
+        margin-top: 20px;
+        top: 0;
+        left: 0;
+	}
     div {
-        height: 60px;
-        background-color: var(--placeholder-bg-color);
-        width: 100%;
+        display: flex;
+        gap: 0;
+    }
+	button {
+		padding: 7.5px 15px;
+        border: var(--default-border);
+        background-color: var(--default-blur-color);
+        color: var(--default-text-color);
+        backdrop-filter: blur(10px);
+        cursor: pointer;
+        transition: all 0.15s ease-in-out;
+        font-size: 0.875rem;
+    }
+    button:hover {
+        background-color: var(--default-blur-hover-color);
+    }
+    button:first-child {
+        border-right: none;
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+    }
+    button:last-child {
+        border-left: none;
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
     }
 </style>
