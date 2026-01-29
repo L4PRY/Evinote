4e8c625fb3b06d86cda834c88c925e850dc23170
Patrik <patrikdravucz@gmail.com>
2026-01-29 23:44:47 +0100

    navbar design extensions

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,54 +1,134 @@
 <script lang="ts">
+    let scrollY = 0;
+
+    function navbarExpand() {
+        const nav = document.querySelector('nav');
+        if (scrollY > 50) {
+            nav?.style.setProperty('backdrop-filter', 'blur(10px)');
+            nav?.style.setProperty('border', 'var(--default-border)');
+            nav?.style.setProperty('width', 'calc(80vw + 20px)');
+        } else {
+            nav?.style.setProperty('backdrop-filter', 'none');
+            nav?.style.setProperty('border', '1px solid transparent');
+            nav?.style.setProperty('width', '80vw');
+        }
+    }
 </script>
 
-<nav>
-    <div>
-        <button>Explore</button>
-        <button>Dashboard</button>
-        <button>Settings</button>
-    </div>
-	
-</nav>
+<svelte:window bind:scrollY={scrollY} on:scroll={navbarExpand} />
+
+<div class="main">
+	<nav>
+        <div class="logo">
+            Evinote
+        </div>
+		<div class="nav_island">
+			<button>Explore</button>
+			<div class="nav_island_divider"></div>
+			<button>Dashboard</button>
+			<div class="nav_island_divider"></div>
+			<button>Settings</button>
+		</div>
+        <div class="login">
+            <button class="shrink">Login</button>
+        </div>
+	</nav>
+</div>
 
 <style>
+    .main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 200vh;
+    }
 	nav {
 		display: flex;
-        box-sizing: border-box;
+		box-sizing: border-box;
 		align-items: center;
 		justify-content: center;
 		height: 50px;
 		background-color: transparent;
-		width: 100%;
-		position: absolute;
-        margin-top: 20px;
-        top: 0;
-        left: 0;
+		width: 80vw;
+		position: fixed;
+		margin-top: 20px;
+		top: 0;
+        z-index: 10;
+        border-radius: var(--border-radius);
+        transition: all 0.5s ease-in-out;
+        backdrop-filter: none;
+        border: 1px solid transparent;
 	}
-    div {
-        display: flex;
-        gap: 0;
+    .logo {
+        font-size: 1.5rem;
+        position: absolute;
+        left: 20px;
     }
+	.nav_island {
+		display: flex;
+		gap: 0;
+		text-align: center;
+	}
+	.nav_island_divider {
+		width: 1.5px;
+		background: linear-gradient(
+			0deg,
+			var(--default-blur-color) 20%,
+			var(--button-stroke-color) 40%,
+			var(--button-stroke-color) 50%,
+			var(--default-blur-color) 80%
+		);
+		backdrop-filter: blur(10px);
+		border-top: var(--default-border);
+		border-bottom: var(--default-border);
+		cursor: pointer;
+	}
 	button {
-		padding: 7.5px 15px;
-        border: var(--default-border);
-        background-color: var(--default-blur-color);
-        color: var(--default-text-color);
-        backdrop-filter: blur(10px);
-        cursor: pointer;
-        transition: all 0.15s ease-in-out;
-        font-size: 0.875rem;
-    }
-    button:hover {
-        background-color: var(--default-blur-hover-color);
-    }
-    button:first-child {
+		padding: 8px;
+		box-sizing: border-box;
+		width: 100px;
+		border: var(--default-border);
+        border-radius: var(--border-radius);
+		background-color: var(--default-blur-color);
+		color: var(--default-text-color);
+		backdrop-filter: blur(5px);
+		cursor: pointer;
+		transition: all 0.15s ease-in-out;
+		font-size: 0.875rem;
+	}
+	.shrink {
+		width: calc(100% + 5px);
+	}
+	.nav_island button:hover {
+		text-shadow: 0px 0px 20px var(--fancycolor-1);
+		filter: brightness(1.1);
+		width: 110px;
+	}
+	button:active {
+		text-shadow: 0px 0px 20px skyblue;
+	}
+	.nav_island button:first-child {
+		border-right: none;
+		border-top-right-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+	.nav_island button:last-child {
+		border-left: none;
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+	}
+	.nav_island button:not(:first-child):not(:last-child) {
+		border-left: none;
         border-right: none;
-        border-top-left-radius: var(--border-radius);
-        border-bottom-left-radius: var(--border-radius);
+        border-radius: 0;
+	}
+    .login {
+        position: absolute;
+        right: 12px;
     }
-    button:last-child {
-        border-left: none;
-        border-top-right-radius: var(--border-radius);
-        border-bottom-right-radius: var(--border-radius);
-    }
+	.login button {
+		background: var(--fancygradient);
+        border: none;
+	}
 </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    // import backgroundImage from '$lib/assets/6229893.jpg';
+    import backgroundImage from '$lib/assets/evi_bg.png';
+    import { onMount } from 'svelte';
+</script>
+
+<svelte:body></svelte:body>
+
+<div class="background" style="background-image: url({backgroundImage});">
+</div>
+
+<div class="content">
+    <slot />
+    
+</div>
+
+<style>
+    .background {
+        width: 100%;
+        height: 100vh;
+        position: absolute;
+        background-size: cover;
+        background-position: top;
+        filter: brightness(0.7);
+        top: 0;
+        left: 0;
+        margin: 0;
+        z-index: -1;
+        animation: float 6s ease-in-out infinite;
+    }
+
+</style>
