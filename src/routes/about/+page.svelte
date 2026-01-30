6a71f312723aa58afc273170d6f85b58072965fa
davidlados511 <laddav441@logiker.hu>
2026-01-30 01:43:13 +0100

    fixing issues

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -83,7 +83,7 @@
     -webkit-backdrop-filter: blur(15px);   /* Safari támogatás */
     
     /* keret */
-    border: 1px solid rgba(255, 255, 255, 0.1);
+    border: 2px solid rgba(207, 24, 24, 0.1);
     
     /* árnyék */
     box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
