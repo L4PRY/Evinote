ed683868560dbc6ce100da24dfb2243f0effd6d7
Notheisz Mátyás <nodomatyi@gmail.com>
2026-02-06 20:38:23 +0100

    export generatesecurerandomstring

--- src/routes/boards/[id]/+page.server.ts
+++ src/routes/boards/[id]/+page.server.ts
@@ -1,5 +1,10 @@
 export function load({ params }) {
 	const { id } = params;
+
+	// okay so
+	// check for perms if board.type is private
+	// if board.type is public, just return the board
+	// if board.type is unlisted, check for perms if user is logged in, otherwise just return the board
 	return {
 		id
 	};
