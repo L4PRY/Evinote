720165beb2a42f1c2307f39a4423a657f20c45e4
Notheisz Mátyás <nodomatyi@gmail.com>
2026-02-06 19:57:43 +0100

    list all boards by user on dashboard

--- src/routes/(authenticated)/dashboard/+page.server.ts
+++ src/routes/(authenticated)/dashboard/+page.server.ts
@@ -1,6 +1,23 @@
 import * as auth from '$lib/server/auth';
+import * as table from '$lib/server/db/schema';
+import { db } from '$lib/server/db/index';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+import { desc, eq } from 'drizzle-orm';
+
+export const load = async () => {
+	const user = auth.requireLogin();
+	const boards = db
+		.select()
+		.from(table.Board)
+		.where(eq(table.Board.owner, user.id))
+		.orderBy(desc(table.Board.updated));
+
+	return {
+		user,
+		boards
+	};
+};
 
 export const actions: Actions = {
 	logout: async (event) => {
--- src/routes/(authenticated)/dashboard/+page.svelte
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { resolve } from "$app/paths";
+	import { resolve } from '$app/paths';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 	const user = data!.user;
+	const boards = data!.boards;
 </script>
 
 <h1>Hi, {user.username}!</h1>
-<p>Your  user ID is {user.id}.</p>
+<p>Your user ID is {user.id}.</p>
 <p>You are a {user.role}</p>
 <a href={resolve('/dashboard/sessions')} aria-label="view sessions">View sessions</a>
+<p>Your boards:</p>
+{#await boards then resolvedBoards}
+	{#if resolvedBoards.length === 0}
+		<p>You have no boards yet.</p>
+		<a href={resolve('/boards/new')} aria-label="create a new board">create one?</a>
+	{/if}
+	<ul>
+		{#each resolvedBoards as board (board.id)}
+			<li>
+				<a href={resolve(`/boards/${board.id}`)}>{board.name}</a>
+			</li>
+		{/each}
+	</ul>
+{/await}
+
 <form method="post" action="?/logout" use:enhance>
 	<button>Sign out</button>
 </form>
