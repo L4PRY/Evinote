91d30beba301381074bde26b7dd7caeaa814828c
Notheisz Mátyás <nodomatyi@gmail.com>
2026-02-06 10:26:30 +0100

    rename +page.server.ts to +server.ts (oops)

--- src/routes/api/auth/login/+page.server.ts
+++
@@ -1,7 +0,0 @@
-// user sends credentials thru api
-// backend authenticates and sends back a session token
-//  with which they can set a bearer header to access protected routes
-
-export function POST({ request, cookies }) {
-	const { username, password } = await request.json();
-}
