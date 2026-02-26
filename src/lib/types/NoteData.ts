95de27640400d4206b0a6bd8b829a41020d89106
Notheisz Mátyás <nodomatyi@gmail.com>
2026-02-26 11:51:15 +0100

    update NoteData to use Color type

--- src/lib/types/NoteData.ts
+++ src/lib/types/NoteData.ts
@@ -1,12 +1,9 @@
+import type { Color } from './Color';
 import type { File } from './File';
 
 export type NoteData = {
 	title: string | null;
 	position: { x: number; y: number; z: number };
-	color:
-		| { type: 'hex'; value: string }
-		| { type: 'rgb'; value: [r: number, g: number, b: number, a?: number] }
-		| { type: 'hsl'; value: [h: number, s: number, l: number, a?: number] }
-		| { type: 'oklch'; value: [l: number, c: number, h: number, a?: number] };
+	color: Color;
 	content: (string | File)[];
 };
