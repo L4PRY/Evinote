4b2fc2e7f81e29718b6c5234a6cc990b88c82c5e
matyi <motherfucker2012@logiker.hu>
2026-03-16 14:38:00 +0100

    add explicit check for when color is the default value var(--default-bg-color)

--- src/lib/parseColor.ts
+++ src/lib/parseColor.ts
@@ -1,10 +1,13 @@
 import type { Color } from './types/canvas/Color';
 
-export function parseColor(color: Color): string {
+export function parseColor(color: Color | string): string {
+	if (typeof color === 'string') return color;
+	if (typeof color['value'] )
+	if (!color.type || !color.value) throw new Error('Invalid color object');
 	switch (color.type) {
 		case 'oklch': {
 			const [l, c, h, a] = color.value;
-			return `oklch(${l}% ${c}% ${h}deg ${a ? '/ ' + a : ''})`;
+			return `oklch(${l} ${c} ${h}${a ? ' / ' + a : ''})`;
 		}
 		case 'rgb': {
 			const [r, g, b, a] = color.value;
