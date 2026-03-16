a70bc72d568b8c1b3314b0b14b80cd94db35df6a
matyi <motherfucker2012@logiker.hu>
2026-03-16 10:53:41 +0100

    add explicit default to NoteData.color property

--- src/lib/types/canvas/NoteData.ts
+++ src/lib/types/canvas/NoteData.ts
@@ -4,6 +4,6 @@ import type { File } from './File';
 export type NoteData = {
 	title: string | null;
 	position: { x: number; y: number; z: number };
-	color: Color;
+	color: Color | 'var(--default-bg-color)';
 	content: (string | File)[];
 };
