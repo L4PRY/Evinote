ebe4e0800da6492500a2e849a9d732352cbafc03
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-21 22:34:11 +0100

    add initial file proxy api route

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	server: {
+		proxy: {
+			'/proxy': {
+				target: 'http://localhost:5173/api/proxy',
+				changeOrigin: true,
+				rewrite: path => path.replace(/^\/proxy/, '')
+			}
+		}
+	}
+});
