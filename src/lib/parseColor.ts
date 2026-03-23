a234e441289ac6e2cf7bc1223e9fef98e3b92ece
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-23 18:37:28 +0100

    add parseColor tests with fixes

--- src/lib/parseColor.ts
+++ src/lib/parseColor.ts
@@ -2,22 +2,23 @@ import type { Color } from './types/canvas/Color';
 
 export function parseColor(color: Color | string): string {
 	if (typeof color === 'string') return color;
-	if (typeof color['value'] )
 	if (!color.type || !color.value) throw new Error('Invalid color object');
 	switch (color.type) {
 		case 'oklch': {
 			const [l, c, h, a] = color.value;
-			return `oklch(${l} ${c} ${h}${a ? ' / ' + a : ''})`;
+			return `oklch(${l} ${c} ${h}${a !== undefined ? ' / ' + a : ''})`;
 		}
 		case 'rgb': {
 			const [r, g, b, a] = color.value;
-			return `rgb(${r}, ${g}, ${b} ${a ? '/ ' + a : ''})`;
+			return `rgb(${r}, ${g}, ${b}${a !== undefined ? ' / ' + a : ''})`;
 		}
 		case 'hsl': {
 			const [h, s, l, a] = color.value;
-			return `hsl(${h} ${s}% ${l}%) ${a ? '/ ' + a : ''}`;
+			return `hsl(${h} ${s}% ${l}%${a !== undefined ? ' / ' + a : ''})`;
 		}
 		case 'hex':
-			return color.value;
+			const regex = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+			if (!regex.test(color.value)) throw new Error('Invalid hex color value');
+			else return color.value;
 	}
 }
