c2a8072d49e3a7782c5f3f2962cce850e7bcd56d
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-25 19:34:30 +0100

    move /boards/:id pageload to +layout.server.ts

---
+++ src/routes/boards/[id]/+layout.server.ts
@@ -0,0 +1,48 @@
+import { checkLogin } from '$lib/server/auth';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
+import { routeLogger } from '$lib/server/logger';
+import { error } from '@sveltejs/kit';
+import { and, eq } from 'drizzle-orm';
+import { checkAccessPerms } from '$lib/server/perms';
+
+export async function load({ params }) {
+	const { id } = params;
+	const sessionUser = checkLogin();
+
+	routeLogger.info(`Loading board with id ${id}`);
+
+	const board = await db
+		.select()
+		.from(table.Board)
+		.where(eq(table.Board.id, parseInt(id)))
+		.then(res => res[0]);
+
+	if (!board) {
+		routeLogger.warn(`Board with id ${id} not found`);
+		error(404, 'board not found');
+	}
+
+	// get the full user record if logged in
+	const user = sessionUser
+		? await db
+				.select()
+				.from(table.User)
+				.where(eq(table.User.id, sessionUser.id))
+				.then(res => res[0])
+		: null;
+
+	// get perms for current user and board if any
+
+	const perms = user
+		? await db
+				.select()
+				.from(table.Permissions)
+				.where(and(eq(table.Permissions.bid, parseInt(id)), eq(table.Permissions.uid, user.id)))
+				.then(res => res[0])
+		: null;
+
+	return checkAccessPerms(board, user, perms)
+		? { id, user, board, perms }
+		: error(403, 'forbidden');
+}
