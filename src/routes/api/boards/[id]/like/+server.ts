e1922d52446dcb2ef67ee7b34e18b404cb8ca481
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-29 21:32:01 +0200

    add multiple like error-handling to POST /api/boards/:id/like

--- src/routes/api/boards/[id]/like/+server.ts
+++ src/routes/api/boards/[id]/like/+server.ts
@@ -7,28 +7,26 @@ import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
 import { routeLogger } from '$lib/server/logger';
 import { getBoard } from '$lib/server/perms';
-import { and, count, eq } from 'drizzle-orm';
+import { and, count, DrizzleQueryError, eq } from 'drizzle-orm';
+import type { PostgresError } from 'postgres';
 
 export const POST = async ({ params, locals }) => {
 	const { id } = params;
 	const { board, likes } = await getBoard(id);
 	const user = requireLogin();
 
-	const existingLike = await db
-		.select()
-		.from(table.BoardLikes)
-		.where(and(eq(table.BoardLikes.board, board.id), eq(table.BoardLikes.user, user.id)))
-		.then(res => res[0]);
-
-	if (existingLike) {
-		return new Response(JSON.stringify({ error: 'Already liked' }), { status: 400 });
+	try {
+		await db.insert(table.BoardLikes).values({ board: board.id, user: user.id });
+	} catch (err) {
+		const pgError = (err as DrizzleQueryError).cause as PostgresError; // lol..,,
+		console.log(pgError);
+		if (pgError.code === '23505') {
+			routeLogger.warn(`User ${user.id} attempted to like board ${board.id} multiple times`);
+			return new Response(JSON.stringify({ error: 'Already liked' }), { status: 400 });
+		}
 	}
 
-	let newLikes: number;
-
-	await db.insert(table.BoardLikes).values({ board: board.id, user: user.id });
-
-	newLikes = await db
+	const newLikes = await db
 		.select({ count: count() })
 		.from(table.BoardLikes)
 		.where(eq(table.BoardLikes.board, board.id))
@@ -42,23 +40,11 @@ export const DELETE = async ({ params, locals }) => {
 	const { board, likes } = await getBoard(id);
 	const user = requireLogin();
 
-	const existingLike = await db
-		.select()
-		.from(table.BoardLikes)
-		.where(and(eq(table.BoardLikes.board, board.id), eq(table.BoardLikes.user, user.id)))
-		.then(res => res[0]);
-
-	if (!existingLike) {
-		return new Response(JSON.stringify({ error: 'Like not found' }), { status: 400 });
-	}
-
-	let newLikes: number;
-
 	await db
 		.delete(table.BoardLikes)
 		.where(and(eq(table.BoardLikes.board, board.id), eq(table.BoardLikes.user, user.id)));
 
-	newLikes = await db
+	const newLikes = await db
 		.select({ count: count() })
 		.from(table.BoardLikes)
 		.where(eq(table.BoardLikes.board, board.id))
