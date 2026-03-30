1400002fae35783288d0970e8ec3c3fdaa63654e
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-30 13:06:28 +0200

    add created to board table && rename user creation date

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -4,14 +4,17 @@ import { eq } from 'drizzle-orm';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function GET() {
-    const users = await db.select({
-        id: table.User.id,
-        username: table.User.username,
-        email: table.User.email,
-        created_at: table.User.createdAt,
-    }).from(table.User);
+	const users = await db
+		.select({
+			id: table.User.id,
+			username: table.User.username,
+			email: table.User.email,
+			created_at: table.User.created
+		})
+		.from(table.User);
 
-    return new Response(JSON.stringify(users), {
-        status: 200, headers: { 'Content-Type': 'application/json' }
-    });
+	return new Response(JSON.stringify(users), {
+		status: 200,
+		headers: { 'Content-Type': 'application/json' }
+	});
 }
