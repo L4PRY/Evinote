04b0f54e8f7f5d8db9a9d37bce17507463bb8261
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-30 15:00:32 +0200

    add url params and all time option selection

--- src/routes/boards/trending/+page.svelte
+++ src/routes/boards/trending/+page.svelte
@@ -3,10 +3,18 @@
 	import { type Board } from '$lib/types/trending/Board';
 	import type { Filter } from '$lib/types/trending/Filter';
 	import { onMount } from 'svelte';
+	import { type PageProps } from './$types';
+	import { page } from '$app/state';
+
+	const params = new URLSearchParams(page.url.searchParams);
+
+	// get filters from query params, or default to 'updated' and 'day'
+	const filter = (params.get('filter') || 'updated') as Filter['type'];
+	const timeframe = (params.get('timeframe') || 'day') as Filter['timeframe'];
 
 	let boards = $state<Board[]>([]);
-	let filterType = $state<Filter['type']>('likes');
-	let filterTimeframe = $state<Filter['timeframe']>('day');
+	let filterType = $state<Filter['type']>(filter);
+	let filterTimeframe = $state<Filter['timeframe']>(timeframe);
 	let limit = $state(20);
 	let offset = $state(0);
 	let expected = $state(0);
@@ -31,9 +39,27 @@
 		}
 		loading = false;
 	}
-
 	onMount(() => {
 		addBoards(limit);
+	});
+
+	$effect(() => {
+		// dependency on both filters triggers this
+		filterType;
+		filterTimeframe;
+
+		page.url.searchParams.set('filter', filterType);
+		page.url.searchParams.set('timeframe', filterTimeframe);
+
+		// reset state
+		boards = [];
+		offset = 0;
+		expected = 0;
+
+		// schedule reload
+		queueMicrotask(() => {
+			addBoards(limit);
+		});
 	});
 </script>
 
@@ -43,6 +69,7 @@
 		<option value="week">Week</option>
 		<option value="month">Month</option>
 		<option value="year">Year</option>
+		<option value="all">All Time</option>
 	</select>
 	<select id="type-select" bind:value={filterType}>
 		<option value="likes">Likes</option>
