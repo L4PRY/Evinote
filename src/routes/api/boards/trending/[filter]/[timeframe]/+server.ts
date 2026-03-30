4f7e24f51348406041df0487fa78dac2c316735e
Notheisz Mátyás <nodomatyi@gmail.com>
2026-03-30 11:46:26 +0200

    remove bad comments

--- src/routes/api/boards/trending/[filter]/[timeframe]/+server.ts
+++ src/routes/api/boards/trending/[filter]/[timeframe]/+server.ts
@@ -1,6 +1,5 @@
 import { db } from '$lib/server/db/index.js';
 import { Board, BoardLikes } from '$lib/server/db/schema.js';
-import { routeLogger } from '$lib/server/logger.js';
 import { type Filter, timeframes, filters } from '$lib/types/trending/Filter';
 import { error } from '@sveltejs/kit';
 import { count, desc, eq, sql, and, SQL } from 'drizzle-orm';
@@ -61,40 +60,14 @@ export const GET = async ({ params, url }) => {
 	const lower = Number(url.searchParams.get('lower')) || 0;
 	const upper = Number(url.searchParams.get('upper')) || 50;
 
-	routeLogger.trace(
-		`got trending request with ${filter} on ${timeframe} with limits ${lower} and ${upper}`
-	);
-
 	// check if filter is in enum
 	if (!timeframes.includes(timeframe)) error(400, 'invalid timeframe');
 	if (!filters.includes(filter)) error(400, 'invalid filter');
 
-	// order by likes || updated
-	// if likes then order by amount of likes within timeframe
-	// if updated then order by most frequently updated board within timeframe
-
-	// const dbFilter =
-	// 	filter === 'likes' ? getLikesWithTimeframe(timeframe) : getUpdatedWithTimeframe(timeframe);
-
-	// const result = await db
-	// 	.select({
-	// 		id: Board.id,
-	// 		title: Board.name,
-	// 		updated: Board.updated,
-	// 		likes: (count(BoardLikes.board))
-	// 	})
-	// 	.from(Board)
-	// 	.leftJoin(BoardLikes, eq(BoardLikes.board, Board.id))
-	// 	.where(dbFilter)
-	// 	.orderBy(filter === 'likes' ? desc(count(BoardLikes.board)) : desc(Board.updated))
-	// 	.groupBy(Board.id)
-	// 	.limit(upper)
-	// 	.offset(lower);
-
 	return new Response(
 		JSON.stringify(
 			filter === 'likes'
-				? await db
+				? await db // based on likes
 						.select({
 							id: Board.id,
 							title: Board.name,
@@ -108,15 +81,13 @@ export const GET = async ({ params, url }) => {
 						.groupBy(Board.id)
 						.limit(upper)
 						.offset(lower)
-				: await db
+				: await db // last updated
 						.select({
 							id: Board.id,
 							title: Board.name,
 							updated: Board.updated
-							// likes: count(BoardLikes.board)
 						})
 						.from(Board)
-						// .leftJoin(BoardLikes, eq(BoardLikes.board, Board.id))
 						.where(and(eq(Board.type, 'Public'), getUpdatedWithTimeframe(timeframe)))
 						.orderBy(desc(Board.updated))
 						.groupBy(Board.id)
