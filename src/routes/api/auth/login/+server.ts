3dd1b94735f84f7c3d22abb76fdf65134e14b9ab
Notheisz Mátyás <nodomatyi@gmail.com>
2026-04-07 20:59:32 +0200

    refactor /api/auth/login/

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -3,53 +3,51 @@ import { db } from '$lib/server/db/index';
 import * as table from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
 import * as auth from '$lib/server/auth';
-import { verify } from '@node-rs/argon2';
-import { validateEmail, validateUsername, validatePassword } from '$lib/parseInput';
-import { env } from '$env/dynamic/private';
-import { scryptSync } from 'node:crypto';
 import { routeLogger } from '$lib/server/logger';
 
 export async function POST(event: RequestEvent) {
-	const { request, cookies } = event;
+	const { request } = event;
+	const body = await request.json();
 
-	const { username, password } = (await request.json()) as { username: string; password: string };
+	const { username, password } = body as { username: string; password: string };
 
-	const results = await db.select().from(table.User).where(eq(table.User.username, username));
-	const existingUser = results.at(0);
-	if (!existingUser) {
+	if (!username || !password) {
+		return new Response(JSON.stringify({ error: 'Invalid username or password format' }), {
+			status: 400
+		});
+	}
+
+	const user = await db
+		.select()
+		.from(table.User)
+		.where(eq(table.User.username, username))
+		.then(r => r.at(0));
+
+	if (!user) {
 		return new Response(JSON.stringify({ error: 'Invalid username or password' }), { status: 400 });
 	}
 
-	if (existingUser.role !== 'Admin') {
+	if (user.role !== 'Admin') {
 		return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
 	}
 
-	const validPassword =
-		env.USE_LEGACY_HASH == '1'
-			? scryptSync(password, 'dev-use-do-not-use-in-prod', 32).toString('hex') ===
-				existingUser.passhash
-			: await verify(existingUser.passhash, password, {
-					memoryCost: 19456,
-					timeCost: 2,
-					outputLen: 32,
-					parallelism: 1
-				});
-
-	if (!validPassword) {
+	if (!(await auth.verifyPassword(user.passhash, password.toString()))) {
 		return new Response(JSON.stringify({ error: 'Invalid username or password' }), { status: 400 });
 	}
 
-	const sessions = await auth.createSession(
-		existingUser.id,
-		request.headers.get('user-agent') ?? 'api',
-		''
-	);
+	const session = await auth
+		.createSession(
+			user.id,
+			request.headers.get('user-agent') ?? 'api',
+			'',
+			new Date(Date.now() + 1000 * 60 * 60 * 24) // 1 day
+		)
+		.then(res => res.at(0));
 
-	if (!sessions || sessions.length === 0) {
+	if (!session) {
+		routeLogger.error('Failed to create session for user ID: ' + user.id);
 		return new Response(JSON.stringify({ error: 'Failed to create session' }), { status: 500 });
 	}
-
-	const session = sessions[0];
 
 	// Beállítjuk a session token-t cookie-ban
 	event.setHeaders({
@@ -59,7 +57,8 @@ export async function POST(event: RequestEvent) {
 	return json(
 		{
 			message: 'Login successful',
-			key: session.token
+			key: session.token,
+			eat: session.eat
 		},
 		{ status: 200 }
 	);
