44f1e66bfc266443d7b252a93c2f6445ccd6f97b
davidlados511 <laddav441@logiker.hu>
2026-04-12 19:08:41 +0200

    adding role to api/user

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -4,13 +4,18 @@ import { eq } from 'drizzle-orm';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function GET() {
-    const users = await db.select({
-        id: table.User.id,
-        username: table.User.username,
-        email: table.User.email
-    }).from(table.User);
+	const users = await db
+		.select({
+			id: table.User.id,
+			username: table.User.username,
+			email: table.User.email,
+			role: table.User.role,
+			///created_at: table.User.created,
+		})
+		.from(table.User);
 
-    return new Response(JSON.stringify(users), {
-        status: 200
-    });
+	return new Response(JSON.stringify(users), {
+		status: 200,
+		headers: { 'Content-Type': 'application/json' }
+	});
 }
